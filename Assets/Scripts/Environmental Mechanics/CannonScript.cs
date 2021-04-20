dff09e96d8846138c195159e9758475df0d29872
HansMeulblok <hansmeulblok4@gmail.com>
2021-04-20 09:42:53 +0200

    made first version of rotation

--- Assets/Scripts/Environmental Mechanics/CannonScript.cs
+++ Assets/Scripts/Environmental Mechanics/CannonScript.cs
@@ -7,11 +7,16 @@ public class CannonScript : MonoBehaviour
     public float shootingInterval;
     public int degreeChange = 45;
     private int currentAngle = 0;
-    public int amountOfAngles;
     public float speed;
     private int minRotation = 0, maxRotation = 180;
     public GameObject pivot;
     private Vector3 newRotation = new Vector3(0,0,0);
+    private bool changeDir = true;
+
+    float waitTime = 2;
+    float elapsedTime = 0;
+    bool lerping = false;
+
 
     private void Start()
     {
@@ -20,16 +25,42 @@ public class CannonScript : MonoBehaviour
     private IEnumerator ChangeAngles()
     {
         //rotate
-        currentAngle += degreeChange;
-        pivot.transform.rotation = Quaternion.Lerp(pivot.transform.rotation, Quaternion.Euler(0, 0, currentAngle), Time.time * speed);
+        if(currentAngle == maxRotation)
+        {
+            changeDir = false;
+        }
+        else if(currentAngle == minRotation)
+        {
+            changeDir = true;
+        }
+
+        if(changeDir)
+        {
+            currentAngle += degreeChange;
+        }
+        else
+        {
+            currentAngle -= degreeChange;
+        }
+
+        lerping = true;
+        yield return new WaitForSeconds(2);
+        lerping = false;
+       
+        
         //shoot
-        Debug.Log("Shoot");
-        Debug.Log(currentAngle);
+
 
         //repeat
         
         yield return new WaitForSeconds(shootingInterval);
         StartCoroutine(ChangeAngles());
     }
- 
+
+    private void Update()
+    {
+        if(lerping)
+        pivot.transform.rotation = Quaternion.Lerp(pivot.transform.rotation, Quaternion.Euler(0, 0, currentAngle), Time.deltaTime * speed);
+    }
+
 }
