49c81465bfe2693e868bad389dc88f03636f6274
HansMeulblok <hansmeulblok4@gmail.com>
2021-04-21 15:22:22 +0200

    Prefab, scene safe, comments

--- Assets/FallingPlatform.cs
+++ Assets/FallingPlatform.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class FallingPlatform : MonoBehaviour
@@ -9,16 +7,19 @@ public class FallingPlatform : MonoBehaviour
 
   public void TriggerPlatform()
   {
+    //swtich bool and get the color
     platformBool = !platformBool;
     platformColor = GetComponent<SpriteRenderer>().color;
 
     if(platformBool)
     {
+      //turn on collider and set the alpha to 100%
         GetComponent<BoxCollider2D>().enabled = true;
         GetComponent<SpriteRenderer>().color = new Color(platformColor.r,platformColor.g, platformColor.b, 1f);
     }
     else
     {
+      //turn off collider and set the alpha to 50%
         GetComponent<BoxCollider2D>().enabled = false;
         GetComponent<SpriteRenderer>().color = new Color(platformColor.r,platformColor.g, platformColor.b, 0.5f);
     }
