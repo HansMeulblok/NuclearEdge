873c17b70184881fa2e379faa2447ff267e1b2ba
HansMeulblok <hansmeulblok4@gmail.com>
2021-04-21 15:20:13 +0200

    Trigger platform done?

--- Assets/FallingPlatform.cs
+++ Assets/FallingPlatform.cs
@@ -5,21 +5,22 @@ using UnityEngine;
 public class FallingPlatform : MonoBehaviour
 {
   public bool platformBool = true;
-
+  private Color platformColor;
 
   public void TriggerPlatform()
   {
     platformBool = !platformBool;
+    platformColor = GetComponent<SpriteRenderer>().color;
 
     if(platformBool)
     {
         GetComponent<BoxCollider2D>().enabled = true;
-        GetComponent<SpriteRenderer>().color.a = 0.5f;
+        GetComponent<SpriteRenderer>().color = new Color(platformColor.r,platformColor.g, platformColor.b, 1f);
     }
     else
     {
         GetComponent<BoxCollider2D>().enabled = false;
+        GetComponent<SpriteRenderer>().color = new Color(platformColor.r,platformColor.g, platformColor.b, 0.5f);
     }
-
   }
 }
