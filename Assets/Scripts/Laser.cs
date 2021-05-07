fa619206285104299a787f95c05d2ebc098a691e
HansMeulblok <hansmeulblok4@gmail.com>
2021-05-07 13:56:28 +0200

    Laser prefab, kills on contact

--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -1,31 +1,70 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 [RequireComponent(typeof(LineRenderer))]
 public class Laser : BaseActivator
 {
+
+    PlayerStatusEffects pse;
+
     public int reflections;
     public float maxLength;
 
     private LineRenderer lineRenderer;
     private Ray2D  ray;
     private RaycastHit2D hit;
-    private Vector2 Direction;
+
+    public bool isActivated = true;
+
+    private float timer;
+    [SerializeField] private float buildUpTime;
+    [SerializeField] private float duration;
+    private float durationTimer;
 
     public override void Activate()
     {
-        throw new System.NotImplementedException();
+        isActivated = !isActivated;
+        ToggleLaser();
     }
 
     private void Awake() 
-    {
-        
+    {    
         lineRenderer = GetComponent<LineRenderer>();
+        ToggleLaser();
     }
 
     private void Update() 
     {
+        if (!isActivated)
+        {
+            timer = 0;
+            durationTimer = 0;
+            return;
+        }
+        else
+        {
+            timer += Time.deltaTime;
+            if(timer < buildUpTime)
+            {
+                //visual feedback
+                lineRenderer.enabled = false;
+                return;
+            }
+            else
+            {
+                lineRenderer.enabled = true;
+                durationTimer += Time.deltaTime;
+
+                if(durationTimer >= duration)
+                {
+                    timer = 0;
+                    durationTimer = 0;
+                }
+            }
+        }
+
         ray = new Ray2D(transform.position, transform.right);
         lineRenderer.positionCount = 1;
         lineRenderer.SetPosition(0, transform.position);
@@ -39,6 +78,14 @@ public class Laser : BaseActivator
                 lineRenderer.SetPosition(lineRenderer.positionCount - 1, hit.point);
                 remainingLength -= Vector3.Distance(ray.origin, hit.point);
                 ray = new Ray2D(hit.point, Vector3.Reflect(ray.direction, hit.normal));
+                if(hit.collider.tag == "Player")
+                {
+                    //Get the PlayerStatusEffects script from the player
+                    pse = hit.collider.GetComponent<PlayerStatusEffects>();
+                    //The player is dead
+                    pse.isDead = true;
+                }
+                    
                 if (hit.collider.tag != "TileMap")
                 break;
 
@@ -49,6 +96,18 @@ public class Laser : BaseActivator
                 lineRenderer.positionCount += 1;
                 lineRenderer.SetPosition(lineRenderer.positionCount - 1, ray.origin + ray.direction * remainingLength);
             }
+        }
+    }
+
+    void ToggleLaser()
+    {
+        if (isActivated)
+        {
+            lineRenderer.enabled = true;
+        }
+        else
+        {
+            lineRenderer.enabled = false;
         }
     }
 }
