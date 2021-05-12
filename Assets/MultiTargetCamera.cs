2e391d89301331606aedfc0771259bcd0fdf8598
HansMeulblok <hansmeulblok4@gmail.com>
2021-05-12 14:51:46 +0200

    Cmaera stuff

---
+++ Assets/MultiTargetCamera.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class MultiTargetCamera : MonoBehaviour
+{
+    public List<Transform> targets = new List<Transform>();
+    public Vector3 offset;
+    public float smoothTime = 0.5f;
+
+    public float minZoom = 40f;
+    public float maxZoom = 10f;
+    public float zoomLimit = 50;
+
+    private Vector3 velocity;
+    private new Camera camera;
+
+    private void Start()
+    {
+        camera = GetComponent<Camera>();
+    }
+
+    private void LateUpdate()
+    {
+        if (targets.Count == 0)
+            return;
+
+        CameraMove();
+        CameraZoom();
+    }
+
+    private void CameraMove()
+    {
+        Vector3 middlePoint = GetMiddlePoint();
+        Vector3 newPos = middlePoint + offset;
+        transform.position = Vector3.SmoothDamp(transform.position, newPos, ref velocity, smoothTime);
+    }
+
+    private void CameraZoom()
+    {
+        float newZoom = Mathf.Lerp(maxZoom, minZoom, GetGreatestDistance() / zoomLimit);
+        camera.orthographicSize = Mathf.Lerp(camera.orthographicSize, newZoom, Time.deltaTime);
+    }
+
+    float GetGreatestDistance()
+    {
+        var bounds = new Bounds(targets[0].position, Vector3.zero);
+
+        for (int i = 0; i < targets.Count; i++)
+        {
+            bounds.Encapsulate(targets[i].position);
+        }
+
+        return bounds.size.x;
+    }
+
+    Vector3 GetMiddlePoint()
+    {
+        if(targets.Count == 1)
+        {
+            return targets[0].position;
+        }
+
+        var bounds = new Bounds(targets[0].position, Vector3.zero);
+        for (int i = 0; i < targets.Count; i++)
+        {
+            bounds.Encapsulate(targets[i].position);
+        }
+
+        return bounds.center;
+    }
+}
