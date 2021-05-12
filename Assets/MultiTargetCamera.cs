e6392a1897a54c6d59217e40e0ba493e087a4a02
HansMeulblok <hansmeulblok4@gmail.com>
2021-05-12 15:27:02 +0200

    comments in camera script

--- Assets/MultiTargetCamera.cs
+++ Assets/MultiTargetCamera.cs
@@ -32,6 +32,7 @@ public class MultiTargetCamera : MonoBehaviour
 
     private void CameraMove()
     {
+        //move camera according to transforms in list
         Vector3 middlePoint = GetMiddlePoint();
         Vector3 newPos = middlePoint + offset;
         transform.position = Vector3.SmoothDamp(transform.position, newPos, ref velocity, smoothTime);
@@ -39,12 +40,15 @@ public class MultiTargetCamera : MonoBehaviour
 
     private void CameraZoom()
     {
+        //zoom in based on distance greatest distance between targets
         float newZoom = Mathf.Lerp(maxZoom, minZoom, GetGreatestDistance() / zoomLimit);
         camera.orthographicSize = Mathf.Lerp(camera.orthographicSize, newZoom, Time.deltaTime);
     }
 
     float GetGreatestDistance()
     {
+
+        // calculate distance between targets, return biggest
         var bounds = new Bounds(targets[0].position, Vector3.zero);
 
         for (int i = 0; i < targets.Count; i++)
@@ -57,6 +61,7 @@ public class MultiTargetCamera : MonoBehaviour
 
     Vector3 GetMiddlePoint()
     {
+        //gets the middle point between all targets in bounds
         if(targets.Count == 1)
         {
             return targets[0].position;
