81790da0bcfe1230c1f77ba65f83d4c7648426b8
Emre Karay <emre.karay@hva.nl>
2021-05-16 16:01:20 +0200

    Removed death sync and added player color change

--- Assets/Scripts/Multiplayer/PlayerManager.cs
+++ Assets/Scripts/Multiplayer/PlayerManager.cs
@@ -10,8 +10,8 @@ public class PlayerManager : MonoBehaviourPunCallbacks, IPunObservable
     private MultiTargetCamera multiTargetCamera;
 
     private bool isRendered = false;
-    List<string> deadPlayers = new List<string>();
-    List<PhotonView> players = new List<PhotonView>();
+    //List<string> deadPlayers = new List<string>();
+    //List<PhotonView> players = new List<PhotonView>();
 
     // [SerializeField] private Transform playerParent;
     private Vector2 networkPosition;
@@ -39,13 +39,13 @@ public class PlayerManager : MonoBehaviourPunCallbacks, IPunObservable
         playerSprite = GetComponent<SpriteRenderer>();
         multiTargetCamera = FindObjectOfType<MultiTargetCamera>();
 
+        Invoke("ChangePlayersColor", 0.6f);
 
-        for (int i = 0; i < multiTargetCamera.targets.Count; i++)
-        {
-            players.Add(multiTargetCamera.targets[i].gameObject.GetComponent<PhotonView>());
-        }
+        //for (int i = 0; i < multiTargetCamera.targets.Count; i++)
+        //{
+        //    players.Add(multiTargetCamera.targets[i].gameObject.GetComponent<PhotonView>());
+        //}
     }
-
 
     public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
     {
@@ -84,30 +84,42 @@ public class PlayerManager : MonoBehaviourPunCallbacks, IPunObservable
     private void Update()
     {
         // Remove player from target list and disable player when out of camera FoV
-        if (isRendered && !playerSprite.isVisible && photonView.IsMine)
+        if (isRendered && !playerSprite.isVisible) // && photonView.IsMine
         {
             multiTargetCamera.targets.Remove(transform);
+            gameObject.SetActive(false);
 
-            string playerId = PhotonNetwork.LocalPlayer.UserId;
-            deadPlayers.Add(playerId);
-            PhotonNetwork.CurrentRoom.SetCustomProperties(new Hashtable() { { "DeadPlayers", deadPlayers } });
+            //string playerId = PhotonNetwork.LocalPlayer.UserId;
+            //deadPlayers.Add(playerId);
+            //PhotonNetwork.CurrentRoom.SetCustomProperties(new Hashtable() { { "DeadPlayers", deadPlayers } });
         }
     }
 
-    public override void OnRoomPropertiesUpdate(Hashtable propertiesThatChanged)
-    {
-        if (propertiesThatChanged["DeadPlayers"] != null)
-        {
-            deadPlayers = (List<string>)propertiesThatChanged["DeadPlayers"];
-            foreach (PhotonView player in players)
-            {
-                if (deadPlayers.Contains(player.ViewID.ToString())){
-                    player.gameObject.SetActive(false);
-                }
-            }
-        };
-    }
+    //public override void OnRoomPropertiesUpdate(Hashtable propertiesThatChanged)
+    //{
+    //    if (propertiesThatChanged["DeadPlayers"] != null)
+    //    {
+    //        deadPlayers = (List<string>)propertiesThatChanged["DeadPlayers"];
+    //        foreach (PhotonView player in players)
+    //        {
+    //            if (deadPlayers.Contains(player.ViewID.ToString())){
+    //                player.gameObject.SetActive(false);
+    //            }
+    //        }
+    //    };
+    //}
 
+
+    public void ChangePlayersColor()
+    {
+        int i = 0;
+        Color[] playerColor = { Color.green, Color.red, Color.blue, Color.yellow };
+        foreach (Transform player in multiTargetCamera.targets)
+        {
+            player.GetComponent<SpriteRenderer>().color = playerColor[i];
+            i++;
+        }
+    }
 
     // Check if player is rendered
     private void OnBecameVisible()
