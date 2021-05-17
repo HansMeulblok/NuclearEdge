9bafa53b248ff152b88695e46039845b0f12a823
HansMeulblok <hansmeulblok4@gmail.com>
2021-05-17 14:24:30 +0200

    Project clean up

--- Assets/Scripts/Multiplayer/PlayerManager.cs
+++ Assets/Scripts/Multiplayer/PlayerManager.cs
@@ -19,21 +19,6 @@ public class PlayerManager : MonoBehaviourPunCallbacks, IPunObservable
 
     void Awake()
     {
-        //if (!photonView.IsMine)
-        //{
-        //    Update layer to player two
-        //    foreach (Transform child in playerParent)
-        //    {
-        //        child.gameObject.layer = LayerMask.NameToLayer("PlayerTwo");
-        //    }
-
-        //    Update camera to splitscreen
-        //    Camera camera = playerParent.GetComponentInChildren<Camera>();
-        //    camera.GetComponent<AudioListener>().enabled = false;
-        //    camera.cullingMask = ~(1 << LayerMask.NameToLayer("PlayerOne"));
-        //    camera.rect = new Rect(0, 0, 1, 0.5f);
-        //}
-
         playerRB = GetComponent<Rigidbody2D>();
 
         playerSprite = GetComponent<SpriteRenderer>();
