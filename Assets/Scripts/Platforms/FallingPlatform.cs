d8bd68476e8f001e797611a65ff467d2e9d1ab4d
DESKTOP-9M2UNS6\Manfred Mosterdfiets <wiavre@gmail.com>
2021-05-18 11:51:36 +0200

    Removed editor code in seperate platform scripts; added platform editor script to all platforms; removed unnecessary scripts.

--- Assets/Scripts/Platforms/FallingPlatform.cs
+++ Assets/Scripts/Platforms/FallingPlatform.cs
@@ -5,12 +5,6 @@ using UnityEngine;
 [ExecuteInEditMode]
 public class FallingPlatform : MonoBehaviour
 {
-    [Header("enable/disable editting mode")]
-    public bool editing = true;
-
-    [Header("platform editting variables")]
-    [Range(1f, 10f)] public int platformLength;
-    [Range(1f, 2f)] public int platformHeight;
     public SpriteRenderer spriteHolder;
     public new BoxCollider2D collider;
 
@@ -25,34 +19,11 @@ public class FallingPlatform : MonoBehaviour
 
     private void Start()
     {
-        editing = false;
         initialPosition = transform.position;
     }
 
     private void Update()
     {
-        //if you want to edit the platform enable editing.
-        if (editing)
-        {
-            //prevent NaN errors
-            if (platformHeight == 0 || platformLength == 0)
-            {
-                return;
-            }
-
-            //update sprite and scale
-            transform.localScale = new Vector3(platformLength, platformHeight, transform.localScale.z);
-            spriteHolder.drawMode = SpriteDrawMode.Tiled;
-            spriteHolder.transform.localScale = new Vector3(1, 1, 1);
-            float newLength = spriteHolder.transform.localScale.y / platformLength;
-            float newHeight = spriteHolder.transform.localScale.y / platformHeight;
-            spriteHolder.transform.localScale = new Vector3(newLength, newHeight, 1);
-            spriteHolder.size = new Vector2(platformLength, platformHeight);
-
-            //update the collider
-            collider.enabled = false;
-            collider.enabled = true;
-        }
 
         //start timer if the platform is stepped on
         if (steppedOn)
