1c9f849a69400ac200eadb2e95f55e46a142a29d
Emre Karay <emre.karay@hva.nl>
2021-05-19 14:44:04 +0200

    Changed RPC to callbacks

--- Assets/Scripts/Environmental Mechanics/ButtonTriggers.cs
+++ Assets/Scripts/Environmental Mechanics/ButtonTriggers.cs
@@ -1,33 +1,62 @@
 using UnityEngine;
 using Photon.Pun;
-public class ButtonTriggers : MonoBehaviourPun
+using Photon.Realtime;
+using ExitGames.Client.Photon;
+
+public class ButtonTriggers : MonoBehaviourPun, IOnEventCallback
 {
     [Header("Place gameobject with activator here")]
     public BaseActivator[] activators;
 
-    private void OnEnable()
-    {
-        //if(GetComponent<PhotonView>() != null)
-        //{
-        //    Destroy(GetComponent<PhotonView>());
-        //}  
-        //gameObject.AddComponent<PhotonView>();
-    }
+    public const byte TriggerTraps = 1;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player") && other.GetComponent<PhotonView>().IsMine)
         {
-            photonView.RPC("ActivateTraps", RpcTarget.All);
-
+            //photonView.RPC("ActivateTraps", RpcTarget.All);
+            SendMoveUnitsToTargetPositionEvent();
         }
     }
 
-    [PunRPC]private void ActivateTraps(PhotonMessageInfo info)
+    //[PunRPC]
+    private void ActivateTraps()
     {
         for (int i = 0; i < activators.Length; i++)
         {
             activators[i].Activate();
+        }
+    }
+
+    private void SendMoveUnitsToTargetPositionEvent()
+    {
+        object[] content = new object[] { gameObject.name };
+        RaiseEventOptions raiseEventOptions = new RaiseEventOptions { Receivers = ReceiverGroup.All };
+        PhotonNetwork.RaiseEvent(TriggerTraps, content, raiseEventOptions, SendOptions.SendReliable);
+    }
+
+    private void OnEnable()
+    {
+        PhotonNetwork.NetworkingClient.EventReceived += OnEvent;
+    }
+
+    private void OnDisable()
+    {
+        PhotonNetwork.NetworkingClient.EventReceived -= OnEvent;
+    }
+
+    public void OnEvent(EventData photonEvent)
+    {
+        byte eventCode = photonEvent.Code;
+        if (eventCode == TriggerTraps)
+        {
+            object[] tempObject = (object[])photonEvent.CustomData;
+            string trapName = (string)tempObject[0];
+
+            if (trapName == gameObject.name)
+            {
+                ActivateTraps();
+            }
         }
     }
 }
