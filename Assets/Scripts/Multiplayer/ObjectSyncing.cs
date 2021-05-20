7814cde68639c3f8d5e2ee2db5f635677a5fc79f
Emre Karay <emre.karay@hva.nl>
2021-05-20 12:07:21 +0200

    Made a separate script for object syncing

---
+++ Assets/Scripts/Multiplayer/ObjectSyncing.cs
@@ -0,0 +1,68 @@
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+
+public class ObjectSyncing : MonoBehaviourPunCallbacks, IPunObservable
+{
+    [Header("What needs to be sycned of object")]
+    public bool syncPosition = true;
+    public bool syncRotation = true;
+
+    private Rigidbody2D objectRB;
+
+    private Vector2 networkPosition;
+    private float networkRotation;
+
+    [SerializeField]
+    float rotationSmoothness = 100f;
+
+    private void Awake()
+    {
+        if (GetComponent<Rigidbody2D>() != null)
+        {
+            objectRB = GetComponent<Rigidbody2D>();
+        }
+        else
+        {
+            Debug.LogWarning("No rigidbody of " + gameObject.name + " found. You sure you need syncing?");
+        }
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        // Sends information of object to others
+        if (stream.IsWriting)
+        {
+            if (syncPosition)
+            {
+                stream.SendNext(objectRB.position);
+                stream.SendNext(objectRB.velocity);
+            }
+
+            if (syncRotation) { stream.SendNext(objectRB.rotation); }
+        }
+        // Receives information of object to others
+        else
+        {
+            float lag = Mathf.Abs((float)(PhotonNetwork.Time - info.SentServerTime));
+
+            if (syncPosition)
+            {
+                networkPosition = (Vector2)stream.ReceiveNext();
+                objectRB.velocity = (Vector2)stream.ReceiveNext(); networkPosition += objectRB.velocity * lag;
+            }
+
+            if (syncRotation) { networkRotation = (float)stream.ReceiveNext(); }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        // Updates object with new information of others (clients)
+        if (!photonView.IsMine)
+        {
+            if (syncPosition) { objectRB.position = Vector2.MoveTowards(objectRB.position, networkPosition, Time.fixedDeltaTime); }
+            if (syncRotation) { objectRB.MoveRotation(networkRotation + Time.fixedDeltaTime * rotationSmoothness); }
+        }
+    }
+}
--- Assets/Scripts/Multiplayer/PlayerManager.cs
+++ Assets/Scripts/Multiplayer/PlayerManager.cs
@@ -4,61 +4,22 @@ using ExitGames.Client.Photon;
 using System.Collections.Generic;
 using System.Linq;
 
-public class PlayerManager : MonoBehaviourPunCallbacks, IPunObservable
+public class PlayerManager : MonoBehaviourPunCallbacks
 {
-    private Rigidbody2D playerRB;
     private SpriteRenderer playerSprite;
     private MultiTargetCamera multiTargetCamera;
 
     private bool isRendered = false;
     private List<string> deadPlayers;
 
-    private Vector2 networkPosition;
-    private float networkRotation;
-
     void Awake()
     {
-        playerRB = GetComponent<Rigidbody2D>();
-
         playerSprite = GetComponent<SpriteRenderer>();
         multiTargetCamera = FindObjectOfType<MultiTargetCamera>();
 
         deadPlayers = new List<string>();
 
         Invoke("ChangePlayersColor", 0.6f);
-    }
-
-    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
-    {
-        // Sends information of player to others
-        if (stream.IsWriting)
-        {
-            stream.SendNext(playerRB.position);
-            // stream.SendNext(playerRB.rotation);
-            stream.SendNext(playerRB.velocity);
-        }
-        else
-        {
-            // Receives information of other players
-            networkPosition = (Vector2)stream.ReceiveNext();
-            // networkRotation = (float)stream.ReceiveNext();
-            playerRB.velocity = (Vector2)stream.ReceiveNext();
-
-            float lag = Mathf.Abs((float)(PhotonNetwork.Time - info.SentServerTime));
-            networkPosition += playerRB.velocity * lag;
-        }
-    }
-
-    private void FixedUpdate()
-    {
-        // Updates position of others.
-        if (!photonView.IsMine)
-        {
-            playerRB.position = Vector2.MoveTowards(playerRB.position, networkPosition, Time.fixedDeltaTime);
-
-            // Uncomment this and networkRotation variable (above) to enable rotation sync
-            // playerRB.MoveRotation(networkRotation + Time.fixedDeltaTime * 100.0f); 
-        }
     }
 
     private void Update()
