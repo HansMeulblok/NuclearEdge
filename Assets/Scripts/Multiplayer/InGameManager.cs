8e24007810b8712d4973c700374a8c3e3aea5e00
DESKTOP-9M2UNS6\Manfred Mosterdfiets <wiavre@gmail.com>
2021-05-20 15:59:46 +0200

    Exit menu works when only one player left and when player reaches finishline.

--- Assets/Scripts/Multiplayer/InGameManager.cs
+++ Assets/Scripts/Multiplayer/InGameManager.cs
@@ -2,16 +2,29 @@ using Photon.Pun;
 using ExitGames.Client.Photon;
 using TMPro;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 
 public class InGameManager : MonoBehaviourPunCallbacks
 {
     [SerializeField] TMP_Text resultText;
 
+    public GameObject finishedMenu;
+
+    private MultiTargetCamera multiTargetCamera;
+
+    private void Start()
+    {
+        multiTargetCamera = FindObjectOfType<MultiTargetCamera>();
+
+        finishedMenu.SetActive(false);
+    }
     public override void OnRoomPropertiesUpdate(Hashtable propertiesThatChanged)
     {
-        if(propertiesThatChanged["playerWon"] != null)
+        //if player reaches finish line or there is only one player remaining
+        if(propertiesThatChanged["playerWon"] != null || multiTargetCamera.targets.Count == 1)
         {
+            finishedMenu.SetActive(true);
+
             if (propertiesThatChanged["playerWon"].Equals(PhotonNetwork.NickName))
             {
                 resultText.text = "You Win!";
@@ -22,6 +35,13 @@ public class InGameManager : MonoBehaviourPunCallbacks
                 resultText.text = "You Lose!";
                 resultText.color = Color.red;
             }
+
         }
+    }
+
+    public void GoToMenu()
+    {
+        
+        SceneManager.LoadScene("Menu");
     }
 }
