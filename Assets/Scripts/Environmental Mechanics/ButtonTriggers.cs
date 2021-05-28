739e2bfb460ede959ef2e4249b4219a0fe1e51d8
TyroRNG1324 <69843437+TyroRNG1324@users.noreply.github.com>
2021-05-28 16:55:41 +0200

    Remove testing code

--- Assets/Scripts/Environmental Mechanics/ButtonTriggers.cs
+++ Assets/Scripts/Environmental Mechanics/ButtonTriggers.cs
@@ -4,8 +4,6 @@ public class ButtonTriggers : MonoBehaviour
 {
     [Header("Place gameobject with activator here")]
     public BaseActivator[] activators;
-    public bool trigger;
-
 
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -15,18 +13,6 @@ public class ButtonTriggers : MonoBehaviour
             {
                 activators[i].Activate();
             }
-        }
-    }
-
-    private void Update()
-    {
-        if (trigger)
-        {
-            for (int i = 0; i < activators.Length; i++)
-            {
-                activators[i].Activate();
-            }
-            trigger = false;
         }
     }
 }
